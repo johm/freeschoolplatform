fef9ce3ace347d03bda1ad417ebc97e6517c9437
John Duda <john@redemmas.org>
2017-06-25 11:40:33 -0400

    allow adding cpq to site

--- app/controllers/admin/course_proposal_questions_controller.rb
+++ app/controllers/admin/course_proposal_questions_controller.rb
@@ -3,10 +3,29 @@ module Admin
     # To customize the behavior of this controller,
     # simply overwrite any of the RESTful actions. For example:
     #
-    # def index
-    #   super
-    #   @resources = CourseProposalQuestion.all.paginate(10, params[:page])
-    # end
+    def index
+       super
+       @resources = CourseProposalQuestion.where(:site => @site).order(:created_at, :desc)
+    end
+
+    def create 
+      resource = resource_class.new(resource_params)
+      resource.site=@site
+        
+      if resource.save
+        redirect_to(
+          [namespace, resource],
+          notice: translate_with_resource("create.success"),
+                    )
+      else
+        render :new, locals: {
+          page: Administrate::Page::Form.new(dashboard, resource),
+        }
+      end
+
+    end
+
+    
 
     # Define a custom finder by overriding the `find_resource` method:
     # def find_resource(param)
