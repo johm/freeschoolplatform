3e6bc01d0734fce12fe8a033bec30c199fc86d8b
John Duda <john@redemmas.org>
2017-10-22 19:56:35 -0400

    tweak

--- app/assets/javascripts/courses.js
+++ app/assets/javascripts/courses.js
@@ -1,4 +1,4 @@
 
 jQuery('document').ready(function (){
-$("main a[href*='course_collections']").css("font-size","48px").css("text-transform","uppercase").css("background-color","#76CABF").css("color","white").css("display","block").css("text-align","center").css("padding","10px").css("border-radius","10px")
+$("main a[href*='course_collections']").css("font-size","48px").css("text-transform","uppercase").css("background-color","#76CABF").css("color","white").css("display","block").css("text-align","center").css("padding","10px").css("border-radius","10px");
 });
